8a493f93be8e211ca69898f829e7ded514bb6ae2
boggda <black_sec@protonmail.com>
2022-12-15 17:39:06 +0300

    flash loans added

--- contracts/FlashLoans.sol
+++ contracts/FlashLoans.sol
@@ -1,6 +1,101 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract Flashloans {
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IFlashLoanReceiver.sol";
 
+contract FlashLoans is ReentrancyGuard {
+
+    struct stacker {
+        uint256 balance;
+        uint256 timestamp;
+    }
+
+    mapping (address => stacker) balances;
+    mapping (address => mapping(address => stacker)) tokenBalances;
+    uint256 public reward;
+    uint256 public flashLoanFee;
+
+    constructor(uint256 _reward, uint256 _flashLoanFee) {
+        reward = _reward;
+        flashLoanFee = _flashLoanFee;
+    }
+
+    function calcReward(uint256 _balance, uint256 _timestamp, uint256 _now, uint256 _reward) public pure returns(uint256) {
+        uint256 months_passed = (_now - _timestamp) / (30 days);
+        return (_balance * (100 + _reward) ** months_passed) / 100 ** months_passed - _balance;
+    }
+
+    function calcFee(uint256 amount, uint256 _flashLoanFee) public pure returns(uint256) {
+        return amount * _flashLoanFee / 10000;
+    }
+
+    function deposit() external nonReentrant payable {
+        require(balances[msg.sender].balance == 0, "You already deposit ether");
+        balances[msg.sender].balance += msg.value;
+        balances[msg.sender].timestamp = block.timestamp;
+    }
+
+    function deposit(address _token, uint256 amount) external nonReentrant {
+        require(tokenBalances[_token][msg.sender].balance == 0, "You already deposit this token");
+        tokenBalances[_token][msg.sender].balance += amount;
+        tokenBalances[_token][msg.sender].timestamp = block.timestamp;
+        IERC20(_token).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw() external nonReentrant {
+        require(balances[msg.sender].balance != 0, "Nothing to withdraw");
+        uint256 amountToWithdraw = balances[msg.sender].balance 
+            + calcReward(
+                balances[msg.sender].balance,
+                balances[msg.sender].timestamp,
+                block.timestamp,
+                reward
+            );
+        balances[msg.sender].balance = 0;
+        (bool sent, ) = payable(msg.sender).call{value: amountToWithdraw}("");
+        require(sent, "Failed to withdraw");
+    }
+
+    function withdraw(address _token) external nonReentrant {
+        require(tokenBalances[_token][msg.sender].balance != 0, "Nothing to withdraw");
+        uint256 amountToWithdraw = tokenBalances[_token][msg.sender].balance
+        + calcReward(
+                tokenBalances[_token][msg.sender].balance,
+                tokenBalances[_token][msg.sender].timestamp,
+                block.timestamp,
+                reward
+            );
+        tokenBalances[_token][msg.sender].balance = 0;
+        IERC20(_token).transfer(msg.sender, amountToWithdraw);
+    }
+
+    function flashLoan(uint256 amount) external nonReentrant payable {
+        require(balances[msg.sender].balance == 0, "You can't take a flash loan if you've already staked eth");
+        uint256 balanceBefore = address(this).balance;
+        require(amount <= balanceBefore, "Not enough eth in the contract");
+        IFlashLoanReceiver(msg.sender).execute{value: amount}();
+        uint256 balanceAfter = address(this).balance;
+        require(balanceAfter >= balanceBefore + calcFee(amount, flashLoanFee), "Flash loan hasn't been paid back");
+    }
+
+    function flashLoan(uint256 amount, address _token) external nonReentrant payable {
+        require(
+            tokenBalances[_token][msg.sender].balance == 0, 
+            "You can't take a flash loan if you've already staked this token"
+        );
+        require(
+            IERC20(_token).balanceOf(address(this)) >= amount, 
+            "Not enough tokens on the balance of the contract"
+        );
+        uint256 fee = calcFee(amount, flashLoanFee);
+        IERC20(_token).transfer(msg.sender, amount);
+        IFlashLoanReceiver(msg.sender).execute();
+        require(
+            IERC20(_token).allowance(msg.sender, address(this)) == amount + fee,
+            "Repay not approved"
+        );
+        IERC20(_token).transferFrom(msg.sender, address(this), amount + fee);
+    }
 }
---
+++ test/flashloans.js
@@ -0,0 +1,106 @@
+const { expect } = require("chai");
+const hre = require("hardhat");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
+const { ethers } = require("hardhat");
+
+describe("LiquidityPool", function () {
+    let MONTHLY_REWARD, deployer, user1, user2;
+
+    beforeEach(async function () {
+        [deployer, user1, user2] = await ethers.getSigners();
+        MONTHLY_REWARD = "1";
+        FLASH_LOAN_FEE = "50";
+
+        const FL = await hre.ethers.getContractFactory("FlashLoans", deployer);
+        const TT = await hre.ethers.getContractFactory("TestToken", deployer);
+        this.flash_loans = await FL.deploy(MONTHLY_REWARD, FLASH_LOAN_FEE);
+        this.test_token = await TT.deploy();
+
+        await this.test_token.deployed();
+        await this.flash_loans.deployed();
+    });
+
+    it("Should set the right monthly reward", async function () {
+        expect(await this.flash_loans.reward()).to.deep.equal(ethers.BigNumber.from(MONTHLY_REWARD));
+    });
+
+    it("Should deposit and withdraw ether", async function () {
+        await this.flash_loans.connect(user1)["deposit()"]({value: ethers.utils.parseEther("1")});
+        expect(await ethers.provider.getBalance(this.flash_loans.address)).to.deep.equal(ethers.utils.parseEther("1"));
+        await this.flash_loans.connect(user1)["withdraw()"]();
+        expect(await ethers.provider.getBalance(this.flash_loans.address)).to.deep.equal(ethers.utils.parseEther("0"));
+    });
+
+    it("Get eth reward for 1 month", async function () {
+        const ONE_MONTH_IN_SECS = 30*24*60*60;
+        console.log(await user1.getBalance());
+        await this.flash_loans.connect(user1)["deposit()"]({value: ethers.utils.parseEther("1")});
+        await this.flash_loans.connect(user2)["deposit()"]({value: ethers.utils.parseEther("1")});
+        expect(await ethers.provider.getBalance(this.flash_loans.address)).to.deep.equal(ethers.utils.parseEther("2"));
+
+        await time.increaseTo((await time.latest()) + ONE_MONTH_IN_SECS);
+
+        expect(await this.flash_loans.connect(user1)["withdraw()"]()).to.changeEtherBalance(user1, ethers.utils.parseEther("1.01"));
+        console.log(await user1.getBalance());
+    });
+
+    it("Get eth reward for 12 months", async function () {
+        const TWELVE_MONTHS_IN_SECS = 10*30*24*60*60;
+        console.log(await user1.getBalance());
+        await this.flash_loans.connect(user1)["deposit()"]({value: ethers.utils.parseEther("1")});
+        await this.flash_loans.connect(user2)["deposit()"]({value: ethers.utils.parseEther("1")});
+        expect(await ethers.provider.getBalance(this.flash_loans.address)).to.deep.equal(ethers.utils.parseEther("2"));
+
+        await time.increaseTo((await time.latest()) + TWELVE_MONTHS_IN_SECS);
+
+        expect(await this.flash_loans.connect(user1)["withdraw()"]()).to.changeEtherBalance(user1, ethers.utils.parseEther("1.14"));
+        console.log(await user1.getBalance());
+    });
+
+    it("Should deposit and withdraw ether", async function () {
+        await this.test_token.connect(deployer).transfer(user1.address, "100000000000000000000")
+        expect(await this.test_token.balanceOf(user1.address)).to.equal("100000000000000000000");
+        await this.test_token.connect(user1).approve(this.flash_loans.address, "100000000000000000000");
+        await this.flash_loans.connect(user1)["deposit(address,uint256)"](this.test_token.address, "100000000000000000000");
+        expect(await this.test_token.balanceOf(user1.address)).to.equal("0");
+        expect(await this.test_token.balanceOf(this.flash_loans.address)).to.equal("100000000000000000000");
+        await this.flash_loans.connect(user1)["withdraw(address)"](this.test_token.address);
+        expect(await this.test_token.balanceOf(this.flash_loans.address)).to.equal("0");
+    });
+
+    it("Get token reward for 1 month", async function () {
+        const ONE_MONTH_IN_SECS = 30*24*60*60;
+        await this.test_token.connect(deployer).transfer(user1.address, "100000000000000000000")
+        expect(await this.test_token.balanceOf(user1.address)).to.equal("100000000000000000000");
+        console.log(await this.test_token.balanceOf(user1.address));
+        await this.test_token.connect(user1).approve(this.flash_loans.address, "100000000000000000000");
+        await this.flash_loans.connect(user1)["deposit(address,uint256)"](this.test_token.address, "100000000000000000000");
+        await this.test_token.connect(deployer).approve(this.flash_loans.address, "100000000000000000000");
+        await this.flash_loans.connect(deployer)["deposit(address,uint256)"](this.test_token.address, "100000000000000000000");
+        expect(await this.test_token.balanceOf(user1.address)).to.equal("0");
+        expect(await this.test_token.balanceOf(this.flash_loans.address)).to.equal("200000000000000000000");
+
+        await time.increaseTo((await time.latest()) + ONE_MONTH_IN_SECS);
+
+        expect(await this.flash_loans.connect(user1)["withdraw(address)"](this.test_token.address)).to.changeTokenBalance(this.test_token, user1, "101000000000000000000");
+        console.log(await this.test_token.balanceOf(user1.address));
+    });
+
+    it("Get token reward for 12 month", async function () {
+        const TWELVE_MONTHS_IN_SECS = 10*30*24*60*60;
+        await this.test_token.connect(deployer).transfer(user1.address, "100000000000000000000")
+        expect(await this.test_token.balanceOf(user1.address)).to.equal("100000000000000000000");
+        console.log(await this.test_token.balanceOf(user1.address));
+        await this.test_token.connect(user1).approve(this.flash_loans.address, "100000000000000000000");
+        await this.flash_loans.connect(user1)["deposit(address,uint256)"](this.test_token.address, "100000000000000000000");
+        await this.test_token.connect(deployer).approve(this.flash_loans.address, "100000000000000000000");
+        await this.flash_loans.connect(deployer)["deposit(address,uint256)"](this.test_token.address, "100000000000000000000");
+        expect(await this.test_token.balanceOf(user1.address)).to.equal("0");
+        expect(await this.test_token.balanceOf(this.flash_loans.address)).to.equal("200000000000000000000");
+
+        await time.increaseTo((await time.latest()) + TWELVE_MONTHS_IN_SECS);
+
+        expect(await this.flash_loans.connect(user1)["withdraw(address)"](this.test_token.address)).to.changeTokenBalance(this.test_token, user1, "110462212541120451001");
+        console.log(await this.test_token.balanceOf(user1.address));
+    });
+});
